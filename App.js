bdfe7e8a7a0923e4d43a78fb9c99b2de0a1d17c0
jakejones2 <jakejones1998@hotmail.co.uk>
2023-09-13 11:46:43 +0100

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [data, setData] = useState("");
+  // useEffect(() => {
+  //   fetch("http:localhost:3000")
+  //     .then((response) => response.json())
+  //     .then((json) => {
+  //       setData(json);
+  //     });
+  // });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>I made an app can you see</Text>
+      <ScrollView>
+        <Text></Text>
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
